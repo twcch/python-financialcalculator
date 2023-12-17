b175e13374e3bef7b6be97605a3300fd39373396
謝志謙 <twcch1218@gmail.com>
2023-12-17 12:11:55 +0800

    update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
 
     return_of_premium_calculator.set_annualized_standard_unit_premium(20)
 
-    print(return_of_premium_calculator.get_annualized_standard_unit_premium())
+    print(return_of_premium_calculator.calculate_return_of_premium_on_base_sum_amount())
 
 
 if __name__ == '__main__':
