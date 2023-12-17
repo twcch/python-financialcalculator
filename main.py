531a1f9c449edcc0d8ceac3514326890132099ae
謝志謙 <twcch1218@gmail.com>
2023-12-17 13:33:56 +0800

    update

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from service.return_of_premium_calculator_service import ReturnOfPremiumCalculatorService
-from service.date_calculator_service import DateCalculatorService
+from service.date_difference_calculator_service import DateDifferenceCalculatorService
 from datetime import datetime
 
 def main():
@@ -7,7 +7,7 @@ def main():
     p1 = '2022-1-1'
     p2 = '2023-5-1'
 
-    date_calculator_service = DateCalculatorService(p1, p2)
+    date_calculator_service = DateDifferenceCalculatorService(p1, p2)
     print(date_calculator_service.calculate_years_difference(0))
 
 
