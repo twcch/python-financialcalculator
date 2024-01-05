53024db9d0e7cf108541da77b85b47ebd7a30b4e
謝志謙 <twcch1218@gmail.com>
2024-01-05 20:17:12 +0800

    update

--- service/discount_calculator_service.py
+++ service/discount_calculator_service.py
@@ -1,10 +1,15 @@
-class DiscountCalculatorService():
+from abc import ABC, abstractmethod
 
+class DiscountCalculatorService(ABC):
+
+    @abstractmethod
     def calculate_monthly_interest_rate_for_discount(self):
         pass
 
+    @abstractmethod
     def calculate_monthly_discount_rate(self):
         pass
 
+    @abstractmethod
     def calculate_discount_amount(self):
         pass
