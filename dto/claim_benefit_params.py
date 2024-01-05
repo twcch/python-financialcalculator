93526dfa177d0ede36442c62b1775164a21e797b
謝志謙 <twcch1218@gmail.com>
2024-01-05 20:32:26 +0800

    update

--- dto/claim_benefit_params.py
+++ dto/claim_benefit_params.py
@@ -1,10 +1,47 @@
 class ClaimBenefitParams():
 
     def __init__(self):
-        # 貼現
+        # 保額相關
+        self.__vpu = 10000
+        self.__unit_base_sum_amount = 0
+        self.__unit_paid_up_sum_amount = 0
+        self.__multiple_for_sum_amount = 1
+        self.__result_ratio_for_sum_amount = 1
+
+        # 貼現相關
         self.__payment_amount = 0
         self.__annual_interest_rate_for_discount = 0
         self.__discount_times = 0
+
+    def get_vpu(self):
+        return self.__vpu
+
+    def set_vpu(self, vpu):
+        self.__vpu = vpu
+
+    def get_unit_base_sum_amount(self):
+        return self.__unit_base_sum_amount
+
+    def set_unit_base_sum_amount(self, unit_base_sum_amount):
+        self.__unit_base_sum_amount = unit_base_sum_amount
+
+    def get_unit_paid_up_sum_amount(self):
+        return self.__unit_paid_up_sum_amount
+
+    def set_unit_paid_up_sum_amount(self, unit_paid_up_sum_amount):
+        self.__unit_paid_up_sum_amount = unit_paid_up_sum_amount
+
+    def get_multiple_for_sum_amount(self):
+        return self.__multiple_for_sum_amount
+
+    def set_multiple_for_sum_amount(self, multiple_for_sum_amount):
+        self.__multiple_for_sum_amount = multiple_for_sum_amount
+
+    def get_result_ratio_for_sum_amount(self):
+        return self.__result_ratio_for_sum_amount
+
+    def set_result_ratio_for_sum_amount(self, result_ratio_for_sum_amount):
+        self.__result_ratio_for_sum_amount = result_ratio_for_sum_amount
 
     def get_payment_amount(self):
         return self.__payment_amount
