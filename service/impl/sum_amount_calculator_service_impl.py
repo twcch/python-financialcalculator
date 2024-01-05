03983349a69a9f489468823ddc8b5b3a767ca69d
謝志謙 <twcch1218@gmail.com>
2024-01-05 20:55:58 +0800

    update

--- service/impl/sum_amount_calculator_service_impl.py
+++ service/impl/sum_amount_calculator_service_impl.py
@@ -3,4 +3,33 @@ from service.sum_amount_calculator_service import SumAmountCalculatorService
 
 
 class SumAmountCalculatorServiceImpl(SumAmountCalculatorService):
-    pass
+    def __init__(self, calim_benfit_params):
+        self.__claim_benefit_params = calim_benfit_params
+
+    def calculate_sum_amount_on_base_sum_amount(self):
+        result = self.__calculate_sum_amount("B")
+
+        return result
+
+    def calculate_sum_amount_on_paid_up_sum_amount(self):
+        result = self.__calculate_sum_amount("P")
+
+        return result
+
+    def calculate_sum_amount_on_total_sum_amount(self):
+        result = self.__calculate_sum_amount("T")
+
+        return result
+
+    def __calculate_sum_amount(self, var):
+        if var == "B":
+            unit_sum_amount = self.__claim_benefit_params.get_unit_base_sum_amount()
+        if var == "P":
+            unit_sum_amount = self.__claim_benefit_params.get_unit_paid_up_sum_amount()
+        if var == "T":
+            unit_sum_amount = (
+                self.__claim_benefit_params.get_unit_base_sum_amount()
+                + self.__claim_benefit_params.get_unit_paid_up_sum_amount()
+            )
+
+        return unit_sum_amount
--- service/sum_amount_calculator_service.py
+++ service/sum_amount_calculator_service.py
@@ -3,5 +3,13 @@ from abc import ABC, abstractmethod
 
 class SumAmountCalculatorService(ABC):
     @abstractmethod
-    def calculate_monthly_interest_rate_for_discount(self):
+    def calculate_sum_amount_on_base_sum_amount(self):
+        pass
+
+    @abstractmethod
+    def calculate_sum_amount_on_paid_up_sum_amount(self):
+        pass
+
+    @abstractmethod
+    def calculate_sum_amount_on_total_sum_amount(self):
         pass
