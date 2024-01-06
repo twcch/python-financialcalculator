ae9341306fd66f565ed6c2719cc8f7474037ea0f
謝志謙 <twcch1218@gmail.com>
2024-01-06 11:01:01 +0800

    update

--- controller/calculator_controller.py
+++ controller/calculator_controller.py
@@ -3,21 +3,19 @@ from service.calculator.impl.discount_calculator_service_impl import (
     DiscountCalculatorServiceImpl,
 )
 
-import logging
-
 
 class CalculatorController:
     def __init__(self):
-        pass
+        self.__claim_benefit_params = ClaimBenefitParams()
+        self.__claim_benefit_params.set_payment_amount(40000)
+        self.__claim_benefit_params.set_annual_interest_rate_for_discount(0.021)
+        self.__claim_benefit_params.set_discount_times(7)
+
+
 
     def compute_discount(self):
-        claim_benefit_params = ClaimBenefitParams()
-        claim_benefit_params.set_payment_amount(40000)
-        claim_benefit_params.set_annual_interest_rate_for_discount(0.021)
-        claim_benefit_params.set_discount_times(7)
-
         discount_calculator_service_impl = DiscountCalculatorServiceImpl(
-            claim_benefit_params
+            self.__claim_benefit_params
         )
 
         result = discount_calculator_service_impl.calculate_discount_amount()
